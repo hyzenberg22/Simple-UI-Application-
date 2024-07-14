aadbbb6ea192b5ea58a6f6c5378c57b72ccb1a91
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-14 16:34:18 +0530

    Completing the 1st user Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 //This is the mail application component inside this we have to import the other nestet component
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./Header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
