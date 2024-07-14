7ff57cd4093b307d073c093e02d8825a15618b88
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-14 15:21:16 +0530

    Adding out custom Header Component

---
+++ src/app/Header/header.component.ts
@@ -0,0 +1,9 @@
+// header component consist the main header image
+import { Component} from '@angular/core';
+
+@Component({
+  selector : 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+// This is the main entry point from where all the apps are running
+// Importing all the components both custom and prebuilt components
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/Header/header.component';
 
+
+
+
+// Running the custom components in the application
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(HeaderComponent).catch((err) => console.error(err));
