675bac4185c01c37c5d7f51bc0dea12e3fbc7b07
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-15 23:41:02 +0530

    change the state management with the signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 //to generate the random number
@@ -12,7 +12,8 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   // it is aviliable in the template, If we make this private then it will bw aviliable to the class only
-  slectetdUsers = DUMMY_USERS[randomIndex]
+  // signal can detect the changes made by the function and also changes in the UI
+  slectetdUsers = signal(DUMMY_USERS[randomIndex]) // set a initial value
 
   //We are making a getter for the computaion in the html avater file
   //this keyword as we are using this inside the class
@@ -23,6 +24,6 @@ export class UserComponent {
   //for the event listner button method
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.slectetdUsers = DUMMY_USERS[randomIndex];
+    this.slectetdUsers.set(DUMMY_USERS[randomIndex]); //a new value in the signal is passed
   }
 }
