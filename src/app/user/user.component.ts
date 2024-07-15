ddf6b7b45fe55a8801bb622ae697bc890d232c32
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-15 23:24:15 +0530

    add event listner

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,6 +11,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // it is aviliable in the template, If we make this private then it will bw aviliable to the class only 
+  // it is aviliable in the template, If we make this private then it will bw aviliable to the class only
   slectetdUsers = DUMMY_USERS[randomIndex]
+
+  //We are making a getter for the computaion in the html avater file
+  //this keyword as we are using this inside the class
+  get imagePath() {
+    return 'assets/users/' + this.slectetdUsers.avatar;
+  }
+
+  //for the event listner button method
+  onSelectUser() {
+    console.log('The user is clicked');
+  }
 }
