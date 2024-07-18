dfb3570d6b7b2b33df45d0a23f994dbdd4ff3bc8
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-18 23:31:54 +0530

    remove the old code Add the users dynamically  now for each we have a user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,11 @@
-import { Component, computed, signal} from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
+//---------------------all the Imports----------------------------
+import { Component, input, Input} from '@angular/core';
 
-//to generate the random number
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
+
+
+
+// ---------------------Component Section -----------------------------
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -11,9 +13,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input() avater!:string;
+  @Input() name!:string;
+
+
+  get imagePath(){
+    return 'assets/users/' + this.avater;
+  }
+
+  onSelectUser () {}
+
+}
+
+
+
+//to generate the random number
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// ----------------------old code in the user component class in which we can change the user each click------------
   // it is aviliable in the template, If we make this private then it will bw aviliable to the class only
   // signal can detect the changes made by the function and also changes in the UI
-  slectetdUsers = signal(DUMMY_USERS[randomIndex]) // set a initial value
+  // slectetdUsers = signal(DUMMY_USERS[randomIndex]) // set a initial value
 
   //We are making a getter for the computaion in the html avater file
   //this keyword as we are using this inside the class
@@ -23,11 +42,12 @@ export class UserComponent {
   // }
 
   // now with the signal we can directly use the computed path
-  imagePath = computed(()=> 'assets/users/' + this.slectetdUsers().avatar)
+  // imagePath = computed(()=> 'assets/users/' + this.slectetdUsers().avatar)
 
   //for the event listner button method
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.slectetdUsers.set(DUMMY_USERS[randomIndex]); //a new value in the signal is passed
-  }
-}
+  // onSelectUser() {
+  //   ------------------we don't need the random user to change one click
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.slectetdUsers.set(DUMMY_USERS[randomIndex]); //a new value in the signal is passed
+  // }
+
