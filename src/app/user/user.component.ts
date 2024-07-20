3169327011ca7ab7916c294f1745dec298cbdb75
hyzenberg22 <hyzenberg22@protonmail.com>
2024-07-20 23:23:07 +0530

    Implemented the selected user and also wmit the output of them

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 //---------------------all the Imports----------------------------
-import { Component, input, Input} from '@angular/core';
+import { Component,  EventEmitter,  Input, Output, } from '@angular/core';
 
 
 
@@ -13,15 +13,26 @@ import { Component, input, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avater!:string;
   @Input({required:true}) name!:string;
+  @Output() select = new EventEmitter();
 
 
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+
+  // imagePath = computed(()=>{
+  //   return 'assets/users/' + this.avatar();
+  // })
   get imagePath(){
     return 'assets/users/' + this.avater;
   }
 
-  onSelectUser () {}
+  onSelectUser () {
+    this.select.emit(this.id);
+  }
 
 }
 
