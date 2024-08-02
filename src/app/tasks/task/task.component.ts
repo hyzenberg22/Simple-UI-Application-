7127b8db6c08e76381c1314d56c4f0756af8e00b
hyzenberg22 <hyzenberg22@protonmail.com>
2024-08-03 01:22:36 +0530

    Base of the day 06aug 2024

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
 //Define the task component
 // interface Task {
@@ -19,4 +19,14 @@ import { Task } from './task.model';
 export class TaskComponent {
   // now we have to export the task component and also have to define the task
   @Input({required:true}) task!:Task
+
+
+  //set a event listner property
+  @Output() complete = new EventEmitter();
+
+
+  // adding a method
+  onCompleteTask(){
+    this.complete.emit(this.user.id);
+  }
 }
